c619ea6f3dbb5bb45d51e71529fa3ef7f262ab7a
cgimcas <cesargicas@gmail.com>
2023-05-04 17:04:59 +0200

    Create PersonViewController.java

---
+++ IPC_Project/src/controller/PersonViewController.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+/**
+ * FXML Controller class
+ *
+ * @author cesar
+ */
+public class PersonViewController implements Initializable {
+
+
+    @FXML
+    private TextField nameTextField;
+    @FXML
+    private TextField surnameTextField;
+    @FXML
+    private Button acceptButton;
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+    @FXML
+    private void acceptOnAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void cancelOnAction(ActionEvent event) {
+    }
+
+}
